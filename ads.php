a7bbb244999ee01266036254e1f1baebc0874d4c
Ricardo Hagens <ricardo@onygo.nl>
2014-09-20 00:05:02 +0200

    Small changes

---
+++ ads.php
@@ -0,0 +1,10 @@
+<h6>Advertentie(s)</h6>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Onygo Git 728x90 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-5093036567906925"
+     data-ad-slot="3262332100"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
